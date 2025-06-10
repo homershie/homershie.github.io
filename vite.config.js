256da99770a1ed54d36f0507eabf66b5b68b3f4e
homershie <homershie@gmail.com>
2025-06-10 23:30:18 +0800

    Fix deploy issues#2

--- vite.config.js
+++ vite.config.js
@@ -18,11 +18,11 @@ export default defineConfig({
     assetsDir: 'assets',
     rollupOptions: {
       output: {
-        manualChunks: {
-          vue: ['vue'],
-          'vue-router': ['vue-router'],
-          pinia: ['pinia'],
-        },
+        manualChunks: undefined,
+        format: 'es',
+        entryFileNames: 'assets/[name]-[hash].js',
+        chunkFileNames: 'assets/[name]-[hash].js',
+        assetFileNames: 'assets/[name]-[hash].[ext]',
       },
     },
   },
