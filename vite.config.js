8ae0566663777ab45eb4615b4d9e756470e24190
homershie <homershie@gmail.com>
2025-06-12 15:06:22 +0800

    Add PWA Workbox

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 import { VitePluginRadar } from 'vite-plugin-radar'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -15,6 +16,36 @@ export default defineConfig({
       // Google Analytics tag injection
       analytics: {
         id: 'G-8YSG21XKMM',
+      },
+    }),
+    VitePWA({
+      devOptions: {
+        enabled: true,
+      },
+      // 設定自動更新
+      registerType: 'autoUpdate',
+      workbox: {
+        // 清除過期的快取
+        cleanupOutdatedCaches: true,
+        // 快取檔案的路徑
+        globPatterns: ['**/*.{js,css,html,ico,png,jpg,jpeg,svg,webp}'],
+        // 忽略網址參數，不同參數當作相同檔案
+        // a.jpg = a.jpg?fbclid=1234
+        ignoreURLParametersMatching: [/.*/],
+        // 快取策略
+        runtimeCaching: [
+          {
+            urlPattern: /\.(?:png|jpg|jpeg|svg|webp)$/,
+            handler: 'CacheFirst',
+            options: {
+              cacheName: 'images',
+              expiration: {
+                maxEntries: 100,
+                maxAgeSeconds: 60 * 60 * 24 * 30, // 30天
+              },
+            },
+          },
+        ],
       },
     }),
   ],
